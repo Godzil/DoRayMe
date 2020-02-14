8144e38aa067ec214cea3d99cefafba8899dd4d0
Godzil <godzil@godzil.net>
2020-02-14 14:21:40 +0000

    Add an empty main (for now)

---
+++ source/main.cpp
@@ -0,0 +1,17 @@
+/*
+ *  DoRayMe - a quick and dirty Raytracer
+ *
+ *  Main app
+ *
+ *  Created by Manoël Trapier
+ *  Copyright (c) 2020 986-Studio.
+ *
+ */
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("Hello !\n");
+    return 0;
+}
