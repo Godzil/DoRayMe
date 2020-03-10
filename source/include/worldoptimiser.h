5da0c1018236a1d6905b5b061a0c8c42c28bd4f6
Godzil <godzil@godzil.net>
2020-03-10 09:18:15 +0000

    Started working on "world optimiser" base class.
    The idea is to reorganise objets to take use of the them to prevent most object to be intersect if they are not likely to be on the ray path.

---
+++ source/include/worldoptimiser.h
@@ -0,0 +1,20 @@
+/*
+ *  DoRayMe - a quick and dirty Raytracer
+ *  World optimiser header
+ *
+ *  Created by Manoël Trapier
+ *  Copyright (c) 2020 986-Studio.
+ *
+ */
+#ifndef DORAYME_WORLDOPTIMISER_H
+#define DORAYME_WORLDOPTIMISER_H
+
+/* World Optimiser subclasses will created move objcet around to try to optimise the parsing of the world.
+ * This class is abstract to we can implement different type and change at runtime or build time
+ */
+class WorldOptimiser
+{
+
+};
+
+#endif /* DORAYME_WORLDOPTIMISER_H */
